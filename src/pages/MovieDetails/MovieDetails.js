5974a3c4e17d7a9f11350f9bf7a008c2872fc047
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-20 12:30:12 +0100

    md

--- src/pages/MovieDetails/MovieDetails.js
+++ src/pages/MovieDetails/MovieDetails.js
@@ -1,25 +1,40 @@
 import { useParams, useLocation } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
+import { fetchMovieById } from "services/api";
+import { MovieCard } from '';
+import { Image } from '';
 
 const MovieDetails = () => {
   const [movie, setMovie] = useState([]);
   const { movieId } = useParams();
   const location = useLocation();
   const backLinkRef = useRef(location.state?.from ?? '/');
+
+  const { poster_path, title, vote_average, overview, release_date, genres } =
+    movie;
    
     useEffect(() => {
-        // useState()
-        // isLoading
-        // Error
+      async function getMovieDetails() {
+        try {
+          const filmDetails = await fetchMovieById(movieId);
+          setMovie(filmDetails);
+        } catch (error) {
+          console.log('error', error);
+          }
+        }
        }, [movieId]);
 
     return (
       <div>
         <div to={backLinkRef.current}>Go back</div>
         <section>
-          <div>
-            {/* <img src={ } alt={ } /> */}
-          </div>
+          <MovieCard>
+            <Image src={
+              poster_path
+              ? `https://image.tmdb.org/t/p/w300/${poster_path}`
+              : `https://via.placeholder.com/250x375`}
+              alt={`${title}`} />
+          </MovieCard>
         </section>
         
       </div>
