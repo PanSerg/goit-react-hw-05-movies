0a98376855bbdf18e3a864f83c25509ed0532a00
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-22 11:22:50 +0100

    m

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,14 +10,15 @@ import { Cast } from './Cast';
 export const App = () => {
   return (
       <Routes>
-        <Route path="/" element={<Layout/> }>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieDetails />} />
-        <Route path="cast" element={<Cast />} />
-        <Route path="reviews" element={<Reviews />} />
+          <Route path="movies/:movieId" element={<MovieDetails />} >
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+      </Route>
       </Route>
       <Route path="*" element={<Navigate to="/" replace={true} />}/>
-      </Routes>
+      </Routes >
   );
 };
